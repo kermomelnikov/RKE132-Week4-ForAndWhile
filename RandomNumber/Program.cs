9eeaca0cd33af7b12e0fc6303c75f18cfbf5e462
Kermo M <melnikovkermo@gmail.com>
2023-05-09 22:14:28 +0300

    add week 4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.ExceptionServices;
+
+Random rnd = new Random();
+
+int randomSum = 0;  
+
+for (int i = 0; i < 3; i++)
+{
+    int myRandomNum = rnd.Next(0, 11); // 0 - 10
+    randomSum = randomSum + myRandomNum;    
+    Console.WriteLine($"My random number is {myRandomNum}");
+}
+
+Console.WriteLine($"My random sum total is {randomSum}");
